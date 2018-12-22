2530966b3f251febad7f25f88ad22eb6cf8880ff
Carlos Hernandez <carlos.hernandez@connectit.com.mx>
2018-12-21 18:17:27 -0600

    Released version 0.1.2

--- app/Api/User/UserDataApiView.php
+++ app/Api/User/UserDataApiView.php
@@ -8,28 +8,18 @@
 
 namespace App\Api\User;
 
-use App\Consts\Http;
-use App\Core\AppContainer;
 use App\Core\Config;
 use App\Data\Models\UserData;
 use App\Data\Transformers\UserDataTransformer;
 use App\Factories\ResponseFactory;
-use App\Utils\SecurityUtils;
 use DbModels\Entities\User;
 use DbModels\Repositories\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 use League\Fractal\Serializer\ArraySerializer;
-use League\Route\Router;
-use PHPUnit\DbUnit\Database\Connection;
-use PHPUnit\DbUnit\DataSet\IDataSet;
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Tests\ConfigurableConnectionTestTrait;
-use Tests\DbUnitTestCase;
-use Tests\TestUtils;
 
 /**
  * Class UserDataApiView
@@ -62,9 +52,11 @@ class UserDataApiView
     public function __invoke(ServerRequestInterface $request, array $args) : ResponseInterface
     {
         $regId = $args['regId'] ?? null;
+        $regId = !$regId ? $regId : (int)$regId;
 
         $authData = $request->getAttribute('authData');
-        if ($authData['userId'] !== $regId) {
+        $userId = !$authData['userId'] ? null : (int)$authData['userId'];
+        if ($userId !== $regId) {
             $response = \App\Factories\ResponseFactory::buildUnauthorizedJsonResponse();
             $response->getBody()->write(\json_encode(['msg' => 'Invalid user request']));
             return $response;
